47595780ef87414a74d3b91ee00436dd94287ff7
Yohan <yohan-roux@hotmail.fr>
2016-03-31 11:25:53 +0200

    maj du choix de l'elmt à placé dans le gridLayout

--- Application/src/gui/GuiChoiceElmt.java
+++ Application/src/gui/GuiChoiceElmt.java
@@ -2,30 +2,28 @@ package gui;
 
 
 import ctrl.AElement;
-import mod.*;
 import mod.Box;
+import mod.*;
 import mod.Character;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.concurrent.Callable;
 
 /**
  * Created by Yohan on 17/03/2016.
  */
-public class GuiChoiceElmt extends JFrame implements Callable {
+public class GuiChoiceElmt extends JFrame {
 
     private int nbSprite;
     private String nameSpriteEdit;
     private int resolution = 128;
     private GuiChoiceElmt itself;
 
-    private AElement elmtToSubmit;
+    public boolean hasBeenSelected = false;
 
-
-    protected GuiChoiceElmt(GuiEditor g){
+    protected GuiChoiceElmt(GuiEditor g) {
         nbSprite = g.getNameSprite().size();
         nameSpriteEdit = g.getNameSpriteEdit();
         itself = this;
@@ -34,54 +32,52 @@ public class GuiChoiceElmt extends JFrame implements Callable {
         int ScreenHeight = (int) screenSize.getHeight();
         int ScreenWidth = (int) screenSize.getWidth();
 
-        setSize(new Dimension(resolution*4,resolution*((nbSprite/4)+1)));
-        setLocation((ScreenWidth*2)/5,(ScreenHeight*2)/5);
+        setSize(new Dimension(resolution * 4, resolution * ((nbSprite / 4) + 1)));
+        setLocation((ScreenWidth * 2) / 5, (ScreenHeight * 2) / 5);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(2,3));
+        mainPanel.setLayout(new GridLayout(2, 3));
 
-        for (int i = 0; i < nbSprite; i++){
+        for (int i = 0; i < nbSprite; i++) {
             ButtonEdit a = null;
-            ImageIcon img = new ImageIcon(nameSpriteEdit + "/" +g.getNameSprite().get(i) + ".png");
-            switch (g.getNameSprite().get(i)){
+            ImageIcon img = new ImageIcon(nameSpriteEdit + "/" + g.getNameSprite().get(i) + ".png");
+            switch (g.getNameSprite().get(i)) {
                 case "BoxKO":
-                    a = new ButtonEdit(new Box(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Box(), g.getNameSprite().get(i));
                     break;
                 case "Chara":
-                    a = new ButtonEdit(new Character(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Character(), g.getNameSprite().get(i));
                     break;
                 case "Goal":
-                    a = new ButtonEdit(new Goal(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Goal(), g.getNameSprite().get(i));
                     break;
                 case "Ground":
-                    a = new ButtonEdit(new Floor(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Floor(), g.getNameSprite().get(i));
                     break;
                 case "Vide":
-                    a = new ButtonEdit(new Vide(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Vide(), g.getNameSprite().get(i));
                     break;
                 case "Wall":
-                    a = new ButtonEdit(new Wall(),g.getNameSprite().get(i));
+                    a = new ButtonEdit(new Wall(), g.getNameSprite().get(i));
                     break;
             }
             JPanel b = new JPanel();
             a.setIcon(img);
             b.add(a);
+
+            mainPanel.add(b);
             a.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    elmtToSubmit = ((ButtonEdit)e.getSource()).getElmt();
+                    g.setElmtToSubmit(((ButtonEdit) e.getSource()).getElmt());
+                    hasBeenSelected = true;
                     itself.dispose();
+                    g.setSema(false);
                 }
             });
-            mainPanel.add(b);
         }
         this.add(mainPanel);
         setResizable(false);
         setVisible(true);
-    }
-
-    @Override
-    public Object call() throws Exception {
-        return elmtToSubmit;
     }
 }
