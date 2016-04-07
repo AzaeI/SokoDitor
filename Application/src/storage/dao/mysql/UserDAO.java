896f7e14a0964c906c702747a4421db7e1858aa5
azer <test@aetj.fr>
2016-04-07 16:43:17 +0200

    Create User est fait en théorie

--- Application/src/storage/dao/mysql/UserDAO.java
+++ Application/src/storage/dao/mysql/UserDAO.java
@@ -3,6 +3,7 @@ package storage.dao.mysql;
 import storage.bean.User;
 import storage.dao.DAO;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -15,7 +16,7 @@ public class UserDAO extends DAO<User> {
             ResultSet result = this.connection.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Users WHERE id = " + id);
-            if(result.first())
+            if (result.first())
                 user.setId(result.getInt("id"));
             user.setAccount_activated(result.getBoolean("account_activated"));
             user.setMail(result.getString("mail"));
@@ -29,7 +30,24 @@ public class UserDAO extends DAO<User> {
 
     @Override
     public User create(User object) {
-        return null;
+        User user = new User();
+        try {
+            PreparedStatement prepare = this.connection
+                    .prepareStatement(
+                            "INSERT INTO Users (username,mail, password) VALUES(?, ?, ?)"
+                    );
+            prepare.setString(1, object.getUsername());
+            prepare.setString(2, object.getMail());
+            prepare.setString(3, object.getPassword());
+            user.setUsername(object.getUsername());
+            user.setMail(object.getMail());
+            user.setPassword(object.getPassword());
+            prepare.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+
     }
 
     @Override
