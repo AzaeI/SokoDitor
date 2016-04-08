2855c44d7a4f54261e3ee5aeaed1e7b414114708
Xawirses <xawirses@gmail.com>
2016-04-09 00:36:56 +0200

    Ajount beau menu pour choix map à play

--- Application/src/launcher/Player.java
+++ Application/src/launcher/Player.java
@@ -1,5 +1,6 @@
 package launcher;
 
+import gui.GuiEditor;
 import gui.GuiGame;
 import gui.GuiMap;
 import mod.Game;
@@ -16,43 +17,105 @@ import static launcher.ComponentSettings.MENU_BUTTON_TEXT;
 
 public class Player extends JPanel {
 
-    private ArrayList<JButton> buttons = new ArrayList<JButton>();
+    private int affiche = 1;
 
     public Player(JPanel cards) {
 
-        CardLayout cl               = (CardLayout) cards.getLayout();
-        JButton backToMenuButton    = new JButton(MENU_BUTTON_TEXT);
+        CardLayout cl = (CardLayout) cards.getLayout();
 
-        this.setLayout(new GridLayout(5,0));
-
-        backToMenuButton.addActionListener(e -> cl.show(cards, MENU_BUTTON_TEXT));
-
-        buttons.add(backToMenuButton);
+        JButton buttonReturn = new JButton(ComponentSettings.RETURN_BUTTON_TEXT);
+        JButton buttonPrec = new JButton("Maps Precedentes");
+        JButton buttonSuiv = new JButton("Maps Suivantes");
 
         String[] levelNameList = new File("Levels").list();
 
+        JButton maps[] = new JButton[levelNameList.length+3];
+        maps[0] = buttonReturn;
+        maps[1] = buttonPrec;
+        maps[maps.length-1] = buttonSuiv;
+
         for(int i = 0; i < levelNameList.length; i++) {
             if (levelNameList[i].endsWith(".xml")) {
-                buttons.add(new JButton(levelNameList[i].substring(0, levelNameList[i].length() - 4)));
-                buttons.get(i+1).addActionListener(e -> new GuiGame(new Game(new mod.Map(((JButton)e.getSource()).getText()))));
+                maps[i+2] = new JButton(levelNameList[i].substring(0, levelNameList[i].length() - 4));
+                maps[i+2].addActionListener(e -> new GuiGame(new Game(new mod.Map(((JButton)e.getSource()).getText()))));
             }
         }
 
-        buttons.forEach(this::add);
 
-        initColor();
+        for (JButton button : maps) {
+            button.setForeground(ComponentSettings.BUTTON_BACKGROUND_COLOR);
+            button.setPreferredSize(new Dimension(500, 100));
+            button.setFont(ComponentSettings.FONT);
+            button.setHorizontalTextPosition(SwingConstants.LEFT);
+            button.setHorizontalAlignment(SwingConstants.LEFT);
+            button.setBorderPainted(false);
+            button.setFocusPainted(false);
+            button.setContentAreaFilled(false);
+            button.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseEntered(java.awt.event.MouseEvent evt) {
+                    button.setForeground(ComponentSettings.FOREGROUND_COLOR);
+                }
 
-    }
-
-    private void initColor(){
-        //Coloration des boutons
-        for (int i = 0; i < buttons.size(); i++) {
-           /* buttons.get(i).setBackground(MainFrame.soko_button_background);
-            buttons.get(i).setForeground(MainFrame.soko_foreground);
-            buttons.get(i).setFont(MainFrame.font);*/
+                public void mouseExited(java.awt.event.MouseEvent evt) {
+                    button.setForeground(ComponentSettings.BUTTON_BACKGROUND_COLOR);
+                }
+            });
         }
 
-        //Coloration du fond du panel
-        //this.setBackground(MainFrame.soko_menu_background);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        for (int i=0; i<maps.length; i++) {
+            gbc.gridy = i;
+            gbc.gridheight = 1;
+            gbc.gridwidth = 1;
+            this.add(maps[i], gbc);
+        }
+
+        this.setBackground(ComponentSettings.MENU_BACKGROUND);
+
+        buttonReturn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(cards, MENU_BUTTON_TEXT);
+            }
+        });
+
+        buttonPrec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                affiche -= 4;
+                display_options(maps);
+            }
+        });
+
+        buttonSuiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                affiche += 4;
+                display_options(maps);
+            }
+        });
+
+        display_options(maps);
+    }
+
+    void display_options(JButton[] options) {
+        for (int i = 1; i< options.length; i++) {
+            options[i].setVisible(false);
+        }
+
+        for (int i = affiche+1; i< options.length && i < affiche+2+3; i++) {
+            options[i].setVisible(true);
+        }
+
+        if(affiche == 1)
+            options[1].setVisible(false);
+        else
+            options[1].setVisible(true);
+        if(affiche+3 >= options.length-3)
+            options[options.length-1].setVisible(false);
+        else
+            options[options.length-1].setVisible(true);
     }
 }
